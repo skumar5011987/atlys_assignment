a1154920488741f392fd550db287002e2775cb8d
sailesh kumar <skumar@petz.net>
2024-07-21 15:46:09 +0530

    handle migrations

---
+++ alembic/versions/bbaf1bda839c_migrations.py
@@ -0,0 +1,26 @@
+"""migrations
+
+Revision ID: bbaf1bda839c
+Revises: 
+Create Date: 2024-07-21 13:46:24.057954
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'bbaf1bda839c'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
